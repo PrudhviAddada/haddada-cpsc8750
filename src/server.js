2bdfa7e9c05732f21e988d2a402470c07fe2c801
PrudhviAddada <112673213+PrudhviAddada@users.noreply.github.com>
2022-10-11 23:44:03 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -17,21 +17,28 @@ const cookieParser = require('cookie-parser');
 app.set('view engine', 'ejs');
 app.use(cookieParser());
 let nextVisitorId = 1;
-let currdate = new Date();
+let visitmessage = "";
 app.get('/', (req, res) => {
   console.log(req.cookies);
+  var visit = req.cookies.visited;
   if(req.cookies['visitorId']){
   res.cookie('visitorId', nextVisitorId);}
-  else
-  res.cookie('visitorId', nextVisitorId++);
-  res.cookie('visited', Date.now().toString());
+  else{
+  res.cookie('visitorId', nextVisitorId++);}
+  if(visit != undefined){
+    visit = Math.floor((Date.now() - req.cookies.visited)/1000);
+    visitmessage = "It has been ${visit} seconds since your last visit";
+  }
+  else {
+  visitmessage = "You have never visited before.";
+  }
+  res.cookie('visited', Date.now());
   res.render('welcome', {
     name: req.query.name || "World",
-    datetime: req.query.datetime || new Date().toLocaleString(),
-    visit: req.query.visit || nextVisitorId,
-    lasttime: req.query.lasttime || Math.round((new Date().getTime() - currdate.getTime()) / 1000),
+    datetime: new Date().toLocaleString(),
+    visit: nextVisitorId,
+    visitmessage: visitmessage
   });
-  currdate = new Date();
 
 });
 
